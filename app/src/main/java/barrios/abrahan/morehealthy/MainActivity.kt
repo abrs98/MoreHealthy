bc1258676d91bd26c5c464983372543fdaa2772c
Carlos Valenzuela <luzbelshin@gmail.com>
2021-07-16 02:54:56 -0700

    ..

--- app/src/main/java/barrios/abrahan/morehealthy/MainActivity.kt
+++ app/src/main/java/barrios/abrahan/morehealthy/MainActivity.kt
@@ -11,37 +11,16 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import barrios.abrahan.morehealthy.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration2: AppBarConfiguration
     lateinit var bottomNav:BottomNavigationView
     lateinit var navController:NavController
     lateinit var drawerLayout: DrawerLayout
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        //setSupportActionBar(binding.appBarMain.toolbar)
-
-        //val drawerLayout: androidx.drawerlayout.widget.DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navLeftView
-        //val navController = findNavController(R.id.nav_host_fragment_content_main)
-
-        appBarConfiguration2 = AppBarConfiguration(
-            setOf(
-                R.id.nav_refactor, R.id.nav_imc, R.id.nav_notifications, R.id.nav_settings
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration2)
-        navView.setupWithNavController(navController)
-
         setContentView(R.layout.activity_main)
         bottomNav = findViewById(R.id.nav_view)
 
@@ -70,8 +49,5 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupBottomNavigation() {
         bottomNav.setupWithNavController(navController)
-
-        setContentView(R.layout.entrenamiento_fragment)
-
     }
 }
